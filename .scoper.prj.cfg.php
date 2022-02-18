dda0d7c377d777e3acfc3e239a0ce50c2076fe69
Mike Taykle <miketaykle@o5p.me>
2022-02-18 10:05:50 -0500

    update

---
+++ .scoper.prj.cfg.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * PHP Scoper config file (project-specific).
+ *
+ * You *CAN* edit this file. It can contain project-specific overrides.
+ * This file is automatically detected by `./scoper.cfg.php` and merged in.
+ *
+ * @see          https://github.com/humbug/php-scoper/blob/master/docs/configuration.md
+ *
+ * @since        1.0.0
+ * @noinspection ALL
+ * phpcs:disableFile
+ */
+return [];
